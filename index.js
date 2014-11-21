7183b1ba726cd8f87511bed34c9d7726c17befbc
taterbase <shankga@gmail.com>
2014-11-20 19:06:56 -0700

    clean up style

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-'use strict';
-
-var _ = require('lodash');
+var _ = require('lodash')
 
 function useFilter(swig, filter) {
-	if (filter === undefined) {
-		return Object.keys(_).forEach(function(action) {
-			if (lodashHas(action))
-				useFilter(swig, action)
-		});
-	} else if (Array.isArray(filter)) {
-		filter.forEach(function(f) {
-			useFilter(swig, f)
-		});
-	}
+  if (filter === undefined) {
+    return Object.keys(_).forEach(function(action) {
+      if (lodashHas(action))
+        useFilter(swig, action)
+    })
+  } else if (Array.isArray(filter)) {
+    filter.forEach(function(f) {
+      useFilter(swig, f)
+    })
+  }
 
-	if (lodashHas(filter))
-		swig.setFilter(filter, _[filter])
-	else
-		throw new Error(filter + " is not a lodash function")
+  if (lodashHas(filter))
+    swig.setFilter(filter, _[filter])
+  else
+    throw new Error(filter + " is not a lodash function")
 }
 
 function lodashHas(functionName) {
-	return (_[functionName] && typeof _[functionName] === 'function')
+  return (_[functionName] && typeof _[functionName] === 'function')
 }
 
 module.exports = {
-	useFilter: useFilter
-};
+  useFilter: useFilter
+}
